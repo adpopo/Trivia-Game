e90e5c7532f7bc5b377b2735922de10ba21765bb
adpopo <hxuanpham@gmail.com>
2017-06-30 23:35:39 -0700

    it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //holder var
 var timerDisplay;
-
+var correct = 0;
 // main game object
 var game = {
 
@@ -12,55 +12,37 @@ var game = {
 	//i = value for correct answer
 	questions: [
 		{
-			q: txt,
-			a: txt,
-			b: txt,
-			c: txt,
-			d: txt,
-			i: "a"
+			q: "Who coded this?",
+			a: "You did",
+			b: "Hoang did",
+			c: "She did it",
+			d: "Idk man"
 		},
 		{
-			q: txt,
-			a: txt,
-			b: txt,
-			c: txt,
-			d: txt,
-			i: "a"
+			q: "Are you sure?",
+			a: "Maybe",
+			b: "No",
+			c: "Yes",
+			d: "....what?"
 		},
 		{
-			q: txt,
-			a: txt,
-			b: txt,
-			c: txt,
-			d: txt,
-			i: "a"
-		},
-		{
-			q: txt,
-			a: txt,
-			b: txt,
-			c: txt,
-			d: txt,
-			i: "a"
-		},
-		{
-			q: txt,
-			a: txt,
-			b: txt,
-			c: txt,
-			d: txt,
-			i: "a"
+			q: "What grade should I get?",
+			a: "A",
+			b: "B",
+			c: "C",
+			d: "F"
 		}],
 
 	
 	//current time left
-	time: "30",
+	time: "10",
 	results: [],
 	nowAsking: 0,
 
 	//function - timer
 	timer : function(){
-		timerDisplay = setInterval( displayTime, 1000 );
+		$("#timer").html(game.time);
+		timerDisplay = setInterval( game.displayTime, 1000 );
 	},
 
 	//function - timer display
@@ -72,74 +54,63 @@ var game = {
 		else{
 			//display time
 			game.time--;
+			$("#timer").html(game.time);
 		}
-	},
-
-	//function - timer pause
-	pauseTime : function(){
-		clearInterval(timerDisplay);
-	},
-
-	//function - timer reset
-	resetTime : function(){
-		game.time = 30;
-		//display time
 	},
 
 	//function - show question
-	displayQuestion : function(question){
-		//print question
-		//print options
-	}
+	displayQuestions : function(questions){
+	//print question 1
+	$("#contents").append("<h2>" + questions[0].q + "</h2><br>");
+	//print options
+	$("#contents").append("<input type='radio' name='1' value='0'> "	+ questions[0].a +
+		"<br><input type='radio' class='correct' name='1' value='1'>" + questions[0].b +
+		"<br><input type='radio' name='1' value='0'>" + questions[0].c +
+		"<br><input type='radio' name='1' value='0'>" + questions[0].d +"<br>"
+		);
 
-	//function - display action for mouse hover
-	onHover : function(){
-		//display hover
+	//print question 2
+	$("#contents").append("<h2>" + questions[1].q + "</h2><br>");
+	//print options
+	$("#contents").append("<input type='radio' name='2' value='0'> "	+ questions[1].a +
+		"<br><input type='radio' name='2' value='0'>" + questions[1].b +
+		"<br><input type='radio' class='correct' name='2' value='1'>" + questions[1].c +
+		"<br><input type='radio' name='2' value='0'>" + questions[1].d + "<br>"
+		);
+
+	//print question 3
+	$("#contents").append("<h2>" + questions[2].q + "</h2><br>");
+	//print options
+	$("#contents").append("<input type='radio' class='correct' name='3' value='1'> "	+ questions[2].a +
+		"<br><input type='radio' name='3' value='0'>" + questions[2].b +
+		"<br><input type='radio' name='3' value='0'>" + questions[2].c +
+		"<br><input type='radio' name='3' value='0'>" + questions[2].d +"<br>"
+		);
 	},
-
-	//function - check answer
-	onClick : function(answer){
-		game.pauseTime();
-		if(answer == questions[nowAsking].i){
-			game.correct();
-		}
-		else{
-			game.inCorrect();
-		}
-	},
-
-	//correct display
-	correct : function(){
-		//display correct
-		game.resetTime;
-		if(nowAsking==4){
-			game.endGame();
-		}
-		else{
-		var timeEvent = setTimeout(displayQuestion, delay);
-		}
-	}
-
-	//incorrect display
-	inCorrect : function(){
-		//display incorrect
-		//reset
-		//countdown
-			//newgame
-	}
 
 	//timeup
 	timeUp : function(){
-		//display time up
-		//reset
-		//countdown
-			//newgame
-			//if out of questions, end game
-	}
+		//stop time
+		clearInterval(timerDisplay);
+		//calculate results
+		if(parseInt($('input[name=1]:checked').val()) > 0)
+			correct += parseInt($('input[name=1]:checked').val());
 
-	//answered last question
-	endGame : function(){
-		//display results
-	}
+		if(parseInt($('input[name=2]:checked').val()) > 0)
+		correct += parseInt($('input[name=2]:checked').val());
 
+		if(parseInt($('input[name=3]:checked').val()) > 0)
+		correct += parseInt($('input[name=3]:checked').val());
+
+		//show results
+		$("#contents").empty();
+		$("#contents").append("<h2>Number Correct: " + correct + "</h2><br>");
+		$("#contents").append("<h2>Number Incorrect: " + (3-correct) + "</h2>");
+	}
 };
+
+$("#startBtn").click(function() {
+	$("#contents").empty();
+	game.timer();
+	game.displayQuestions(game.questions);
+});
